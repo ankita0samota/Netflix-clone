1e06980b9223f90ba84ce845ea72931c6da4c07f
Ankita <ankita0samota@gmail.com>
2025-08-18 19:02:35 +0530

    added thumbnail and footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,10 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/Play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -17,8 +21,26 @@ const Home = () => {
             in modern Istanbul embarks on a quest to save the city from an
             immortal enemy.
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More_info
+            </button>
+          </div>
+          <TitleCards />
         </div>
       </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics For You"} />
+      </div>
+      <Footer />
     </div>
   );
 };
